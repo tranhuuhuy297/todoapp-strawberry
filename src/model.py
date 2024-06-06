76fb2bbb939125df537c9497c5595869c74d5705
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2024-06-06 23:06:14 +0700

    add mutation add, delete, update todo

--- src/model.py
+++ src/model.py
@@ -12,3 +12,9 @@ list_todo = [
     Todo(name="Todo #2", done=False),
     Todo(name="Todo #3", done=True)
 ]
+
+
+@strawberry.input
+class AddTodoInput:
+    name: str = strawberry.field(description="The name of todo")
+    done: bool = strawberry.field(description="The status of todo")
--- src/mutation.py
+++ src/mutation.py
@@ -1,10 +1,29 @@
 import strawberry
 
-from src.model import Todo
+from src.model import AddTodoInput, Todo, list_todo
 
 
 @strawberry.type
 class Mutation:
     @strawberry.mutation
-    def add_todo(self, name: str, done: bool) -> Todo:
-        return Todo(name=name, done=done)
+    def add_todo(self, todo: AddTodoInput) -> Todo:  # AddTodoInput for testing
+        if todo.name in [t.name for t in list_todo]:
+            return None
+        list_todo.append(todo)
+        return todo
+
+    @strawberry.mutation
+    def update_todo(self, name: str, done: bool) -> Todo:
+        for todo in list_todo:
+            if todo.name == name:
+                todo.done = done
+                return todo
+        return None
+
+    @strawberry.mutation
+    def delete_todo(self, name: str) -> Todo:
+        for i, todo in enumerate(list_todo):
+            if todo.name == name:
+                list_todo.pop(i)
+                return todo
+        return None
